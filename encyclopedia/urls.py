ee9ea5559f45f6495b21fbe318d184db93a76ca7
Jty-123 <57577404+Jty-123@users.noreply.github.com>
2022-04-04 18:29:17 +0800

    Finish two requirements
    Finish requirements: Entry Page and
    Index Page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-
+import imp
+from django.urls import re_path,path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    re_path(r'^wiki/(.+)/$', views.getEntry,name="entry"),
+    #path("", views.index, name="index"),
+    #path("", views.index, name="index"),
+    #path("", views.index, name="index"),
+    #path("", views.index, name="index"),
+    #path("", views.index, name="index"),
+    #path("", views.index, name="index"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,18 @@
+from cgitb import html
+from tkinter import E
+from unicodedata import name
 from django.shortcuts import render
-
+import markdown
 from . import util
-
-
+from django.http import HttpResponse,Http404, HttpResponseNotFound
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def getEntry(request,param):
+    title  = param
+    Entry =util.get_entry(title)
+    if Entry == None:
+        return HttpResponseNotFound("Sorry,You request page is not found!")
+    return HttpResponse(markdown.markdown(Entry))
