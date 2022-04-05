2042dcf50cf19118bceed5ca7af33f3e95147f90
Jty-123 <57577404+Jty-123@users.noreply.github.com>
2022-04-05 22:23:23 +0800

    Complete all demand

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     re_path(r'^wiki/(.+)/$', views.getEntry,name="entry"),
     path("search", views.search, name="search"),
     path("/random", views.getRandomEntry, name="random"),
-    #path("", views.index, name="index"),
-    #path("", views.index, name="index"),
-    #path("", views.index, name="index"),
+    path("/createPage", views.createPage, name="create"),
+    path("createEntry",views.createEntry,name="creatEntry"),
+    re_path(r'^edit/(.+)/$', views.editPage, name="edit"),
+    path("editEntry",views.editEntry,name="editEntry"),
     #path("", views.index, name="index"),
     #path("", views.index, name="index"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -6,7 +6,7 @@ from unicodedata import name
 from django.shortcuts import render
 import markdown
 from . import util
-from django.http import HttpResponse,Http404, HttpResponseNotFound
+from django.http import HttpResponse,HttpResponseNotFound
 import random
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -18,7 +18,10 @@ def getEntry(request,param):
     Entry =util.get_entry(title)
     if Entry == None:
         return HttpResponseNotFound("Sorry,You request page is not found!")
-    return HttpResponse(markdown.markdown(Entry))
+    return render(request, "encyclopedia/show.html", {
+        "Title":title,
+        "Entry":markdown.markdown(Entry)
+    })
 
 def search(request):
     title = request.GET.get("q")
@@ -27,10 +30,49 @@ def search(request):
         return render(request,"encyclopedia/relate.html",{
             "entries":util.list_RelateEntries(title)
         })
-    return HttpResponse(markdown.markdown(Entry))
+    return render(request, "encyclopedia/show.html", {
+        "Title":title,
+        "Entry":markdown.markdown(Entry)
+    })
 
 def getRandomEntry(request):
     EntryList =util.list_entries()
     r = random.randint(0,len(EntryList)-1)
     Entry = util.get_entry(EntryList[r])
-    return HttpResponse(markdown.markdown(Entry))
+    return render(request, "encyclopedia/show.html", {
+        "Title":title,
+        "Entry":markdown.markdown(Entry)
+    })
+
+def createPage(request):
+    return render(request, "encyclopedia/create.html")
+
+def createEntry(request):
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+    Entry = util.get_entry(title)
+    if Entry is not None:
+        return HttpResponse("Error,the Entry already exists!")
+    util.save_entry(title, content)
+    Entry = util.get_entry(title)
+    return render(request, "encyclopedia/show.html", {
+        "Title":title,
+        "Entry":markdown.markdown(Entry)
+    })
+
+def editPage(request,param):
+    title  = param
+    Entry =util.get_entry(title)
+    return render(request, "encyclopedia/edit.html",{
+        "Title":title,
+        "Content":Entry
+    })
+def editEntry(request):
+    content = request.GET.get("content")
+    title= request.GET.get("title")
+    util.save_entry(title, content)
+    Entry = util.get_entry(title)
+    return render(request, "encyclopedia/show.html", {
+        "Title":title,
+        "Entry":markdown.markdown(Entry)
+    })
